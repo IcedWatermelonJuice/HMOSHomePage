5648c7c5b69c1273fc86a14df84ee1adfab95377
IcedWatermelonJuice <87429695+IcedWatermelonJuice@users.noreply.github.com>
2021-08-01 03:49:29 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-﻿require.config({
+require.config({
 	urlArgs: `v=${app.version}`,
 	baseUrl: "js/lib"
 });
@@ -110,7 +110,7 @@ require(['jquery'], function($) {
 				if (that.get('logo')) {
 					$(".logo").html('<img src="' + that.get('logo') + '" />');
 				} else {
-					$(".logo").html('<img src="logo/HarmonyOS_logo(for nightmode).png"/>');
+					$(".logo").html('<img src="logo/HarmonyOS_logo(for nightMode).png"/>');
 				}
 			} else {
 				nightMode.off();
